c4fe7a2bb0070f99cf00a71cffa2ff2f18765fb2
rohitf116 <rrsonawne090909@gmail.com>
2023-05-01 13:08:52 -0700

    user list

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,8 @@ import ShippingScreen from "./screens/ShippingScreen";
 import PaymentScreen from "./screens/PaymentScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
+import UserListScreen from "./screens/UserListScreen";
+//UserListScreen
 const App = () => {
   return (
     <>
@@ -29,6 +31,7 @@ const App = () => {
           <Route path="/shipping" element={<ShippingScreen />} />
           <Route path="/payment" element={<PaymentScreen />} />
           <Route path="/placeorder" element={<PlaceOrderScreen />} />
+          <Route path="/list" element={<UserListScreen />} />
         </Route>
       </Routes>
     </>
--- client/src/screens/UserListScreen.jsx
+++ client/src/screens/UserListScreen.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Table, Button, Container } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../components/Message";
+
+import Loader from "../components/Loader";
+import { getUsers } from "../actions/userActions";
+import { useEffect } from "react";
 
 const UserListScreen = () => {
-  return <div>UserListScreen</div>;
+  const dispach = useDispatch();
+  const userList = useSelector((state) => state.userList);
+  useEffect(() => {
+    dispach(getUsers());
+  }, []);
+  const { loading, error, users } = userList;
+  return (
+    <div>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Container>
+          {users?.map((user) => (
+            <div key={user._id}>
+              <strong> {user.name}</strong>
+            </div>
+          ))}
+        </Container>
+      )}
+    </div>
+  );
 };
 
 export default UserListScreen;
