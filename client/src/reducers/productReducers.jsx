4189f6c404e431f50ddf7a9335f158bedd808765
rohitf116 <rrsonawne090909@gmail.com>
2023-05-20 13:55:00 -0700

    almost done

--- client/src/reducers/productReducers.jsx
+++ client/src/reducers/productReducers.jsx
@@ -29,6 +29,7 @@ import {
   PRODUCT_TOP_RESET,
   PRODUCT_TOP_REQUEST,
 } from "../constants/productContants";
+//h
 export const productListReducer = (state = { products: [] }, action) => {
   switch (action.type) {
     case PRODUCT_LIST_REQUEST:
