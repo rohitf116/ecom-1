e12dbee93c5f94ce13a1069a6cf1521430fdb2d8
rohitf116 <106576603+rohitf116@users.noreply.github.com>
2023-05-20 15:19:02 +0530

    s

--- backend/src/app.js
+++ backend/src/app.js
@@ -38,9 +38,9 @@ app.get("/api/config/paypal", (req, res) =>
 );
 const dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(dirname, "build")));
+  app.use(express.static(path.join(__dirname, "build")));
   app.get("*", (req, res) =>
-    res.sendFile(path.join(dirname, "build", "index.html"))
+    res.sendFile(path.join(__dirname, "build", "index.html"))
   );
 }
 app.listen(port, () => console.log(`Running on port: ${port}`));
